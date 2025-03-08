509cec5b5bdad892a9e1b9adf695b4c328c97e8b
ChristianHopf <cehopf@gmail.com>
2025-03-08 13:01:51 -0500

    isValidDate invalid time tests

--- dueDateCalculator.test.js
+++ dueDateCalculator.test.js
@@ -1,4 +1,4 @@
-const { isValiddate, calculateDueDate } = require("./dueDateCalculator");
+const { isValidDate, calculateDueDate } = require("./dueDateCalculator");
 
 /**
  * Test cases:
@@ -7,9 +7,17 @@ const { isValiddate, calculateDueDate } = require("./dueDateCalculator");
  * - edge cases
  */
 describe("isValidDate", () => {
-  test("time before 9 AM is invalid", () => {});
+  test("time before 9 AM is invalid", () => {
+    const submitDate = new Date("March 8, 2025 03:25:00");
+    const isValidSubmitDate = isValidDate(submitDate);
+    expect(isValidSubmitDate).toBe(false);
+  });
 
-  test("time after 5 PM is invalid", () => {});
+  test("time after 5 PM is invalid", () => {
+    const submitDate = new Date("March 8, 2025 21:25:00");
+    const isValidSubmitDate = isValidDate(submitDate);
+    expect(isValidSubmitDate).toBe(false);
+  });
 
   test("date on saturday or sunday is invalid", () => {});
 });
