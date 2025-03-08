f6a9f619daf6eac22fc19c5309c0772320232f85
ChristianHopf <cehopf@gmail.com>
2025-03-08 11:53:15 -0500

    Document usage of calculateDueDate

--- dueDateCalculator.js
+++ dueDateCalculator.js
@@ -1,3 +1,19 @@
+/**
+ * Usage:
+ * calculateDueDate(submitDate, turnaround) -> Date
+ * Input:
+ *  - submitDate: Date and time the issue is submitted
+ *  - turnaround: Lifetime of the issue, in hours
+ * Output (Date):
+ *  - Due date and time for the issue
+ * Example:
+ * - calculateDueDate(Date("03-08-2025", 1148), 19)
+ *  - Input:
+ *      - submitDate: March 10, 2025, 11:48 AM
+ *      - turnaround: 19 hours
+ *  - Output:
+ *      - dueDate: March 12, 2025, 2:48 PM
+ */
 function calculateDueDate() {
   console.log("calculate");
 }
---
+++ dueDateCalculator.test.js
@@ -0,0 +1,13 @@
+const calculateDueDate = require("./dueDateCalculator");
+
+/**
+ * Test cases:
+ * - invalid input
+ * - basic functionality
+ * - edge cases
+ */
+describe("calculateDueDate", () => {
+  test("invalid submit date throws error", () => {});
+
+  test("time is tracked only over working hours", () => {});
+});
