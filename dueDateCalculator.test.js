0fe2d14f0ce48f149c97bab89a835bc5eca23f07
ChristianHopf <cehopf@gmail.com>
2025-03-08 13:09:36 -0500

    isValidDate tests use valid days

--- dueDateCalculator.test.js
+++ dueDateCalculator.test.js
@@ -8,25 +8,31 @@ const { isValidDate, calculateDueDate } = require("./dueDateCalculator");
  */
 describe("isValidDate", () => {
   test("time before 9 AM is invalid", () => {
-    const submitDate = new Date("March 8, 2025 03:25:00");
+    const submitDate = new Date("March 10, 2025 03:25:00");
     const isValidSubmitDate = isValidDate(submitDate);
     expect(isValidSubmitDate).toBe(false);
   });
 
   test("time after 5 PM is invalid", () => {
-    const submitDate = new Date("March 8, 2025 21:25:00");
+    const submitDate = new Date("March 10, 2025 21:25:00");
     const isValidSubmitDate = isValidDate(submitDate);
     expect(isValidSubmitDate).toBe(false);
   });
 
   test("time after 5 PM, before 6 PM is invalid", () => {
-    const submitDate = new Date("March 8, 2025 17:00:38");
+    const submitDate = new Date("March 10, 2025 17:00:38");
     const isValidSubmitDate = isValidDate(submitDate);
     expect(isValidSubmitDate).toBe(false);
   });
 
   test("time at exactly 5 PM is valid", () => {
-    const submitDate = new Date("March 8, 2025 17:00:00");
+    const submitDate = new Date("March 10, 2025 17:00:00");
+    const isValidSubmitDate = isValidDate(submitDate);
+    expect(isValidSubmitDate).toBe(true);
+  });
+
+  test("time between 9 AM and 5 PM is valid", () => {
+    const submitDate = new Date("March 10, 2025 17:00:00");
     const isValidSubmitDate = isValidDate(submitDate);
     expect(isValidSubmitDate).toBe(true);
   });
