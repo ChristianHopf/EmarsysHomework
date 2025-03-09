489b5cafe2983459d8ea562c3b0ce93769d5029e
ChristianHopf <cehopf@gmail.com>
2025-03-08 23:00:22 -0500

    Move date by remainder hours

--- dueDateCalculator.js
+++ dueDateCalculator.js
@@ -47,14 +47,66 @@ function calculateDueDate(submitDate, turnaround) {
     return null;
   }
 
+  let submitDateObj = new Date(submitDate);
+
   // Perform due date calculation
   // - Divide turnaround by 8 to get number of working days to add
   // - Add remainder
-  let elapsedDays = turnaround / 8;
+  let elapsedDays = Math.floor(turnaround / 8);
   let remainingHours = turnaround % 8;
 
-  let day = new Date(submitDate).getDay();
-  let dueDay = ((day - 1 + elapsedDays) % 5) + 1;
+  // Move date by remaining hours
+  let hour = submitDateObj.getHours();
+
+  // Pass 5 PM: move to next day 9 AM and add the extra hours
+  if (hour + remainingHours > 17) {
+    // Move to 5 PM
+    submitDate += (17 - hour) * 3600000;
+    // Check if Friday, move to Monday, keep minutes and seconds
+    if (submitDateObj.getDay() == 5) {
+      // From 5 PM Friday to 9 AM Monday = 64 hours
+      submitDate += 64 * 3600000;
+    }
+    // Else move to next day
+    else {
+      submitDate += 16 * 3600000;
+    }
+
+    let extra = hour + remainingHours - 17;
+    submitDate += extra * 3600000;
+  } else {
+    submitDate += remainingHours * 3600000;
+  }
+
+  // Add elapsed weeks
+  //   let day = new Date(submitDate).getDay();
+  while (elapsedDays >= 5) {
+    // For every 5 days we want to add, add 7
+    submitDate += 7 * 86400000;
+    elapsedDays -= 5;
+  }
+
+  // Add remaining elapsed days
+  if (elapsedDays > 0) {
+    submitDate += elapsedDays * 86400000;
+  }
+  return new Date(submitDate);
+  //   if (day + elapsedDays > 5) {
+  //     // Move to Friday, skip the weekend, then the rest of elapsedDays
+  //     let moveToFriday = 5 - day;
+  //     submitDate += ((day + elapsedDays) % 5) * 86400000;
+  //     submitDate += 3 * 86400000;
+  //     submitDate += elapsedDays - (day + elapsedDays - 5);
+  //   }
+
+  //   return new Date(submitDate);
+
+  // Move date by remaining days
+
+  // Move date by elapsed days
+  //   let day = new Date(submitDate).getDay();
+  //   let dueDay = ((day - 1 + elapsedDays) % 5) + 1;
+
   // Based on given time and turnaround, add another day if it passes 5 PM
 
   // Return due date
