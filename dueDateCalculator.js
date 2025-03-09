adf421c5df69f4db1f36ea8ebb620c03c8379ba6
ChristianHopf <cehopf@gmail.com>
2025-03-09 00:38:50 -0500

    Better function documentation and some variable names

--- dueDateCalculator.js
+++ dueDateCalculator.js
@@ -1,82 +1,102 @@
-function isValidDate(date) {
+/**
+ * Validates a given date
+ * @param {date} submitDate - unix timestamp of issue submission
+ * @returns {boolean} - returns whether the date is within working hours
+ * Examples:
+ * - isValidDate(Date.parse("March 10, 2025 12:05:36")) -> true
+ * - isValidDate(Date.parse("March 8, 2025 12:05:36")) -> false
+ */
+function isValidDate(submitDate) {
   // submitDate must be
   // - between 9AM and 5PM
   // - between Mon and Fri
-  let submitDate = new Date(date);
-  let hour = submitDate.getHours();
-  let day = submitDate.getDay();
+  let submitDateObj = new Date(submitDate);
+  let hour = submitDateObj.getHours();
+  let day = submitDateObj.getDay();
 
+  // If hour is not between 9 AM and 5 PM, return false
   if (hour < 9 || hour > 17) {
     return false;
   }
   // If hour is 5 PM, time must be exactly 5 PM
   else if (hour == 17) {
-    return submitDate.getMinutes() == 0 && submitDate.getSeconds() == 0;
+    return submitDateObj.getMinutes() == 0 && submitDateObj.getSeconds() == 0;
   }
 
   // Date object days are 0 (Sunday) through 6 (Saturday), so 1 <= day <= 5
   return 1 <= day && day <= 5;
 }
 
-function moveByRemainingHours(submitDate, remainingHours) {
-  let submitDateObj = new Date(submitDate);
-  let hour = submitDateObj.getHours();
-  let day = submitDateObj.getDay();
+/**
+ *
+ * @param {number} dueDate - unix timestamp of the current due date
+ * @param {number} remainingHours - number of hours by which to advance the due date
+ * @returns {number} - the updated due date
+ * Example:
+ * - moveByRemainingHours(Date.parse("March 7, 2025 12:05:36"), 2) -> (timestamp equivalent to March 7, 2025 14:05:36)
+ */
+function moveByRemainingHours(dueDate, remainingHours) {
+  let dueDateObj = new Date(dueDate);
+  let hour = dueDateObj.getHours();
+  let day = dueDateObj.getDay();
 
   if (hour + remainingHours > 17) {
     // Move to 5 PM
-    submitDate += (17 - hour) * 3600000;
+    dueDate += (17 - hour) * 3600000;
     // Check if Friday, move to Monday, keep minutes and seconds
     if (day == 5) {
       // From 5 PM Friday to 9 AM Monday = 64 hours
-      submitDate += 64 * 3600000;
+      dueDate += 64 * 3600000;
     }
     // Else move to next day
     else {
-      submitDate += 16 * 3600000;
+      dueDate += 16 * 3600000;
     }
 
     let extra = hour + remainingHours - 17;
-    submitDate += extra * 3600000;
+    dueDate += extra * 3600000;
   } else {
-    submitDate += remainingHours * 3600000;
+    dueDate += remainingHours * 3600000;
   }
 
-  return submitDate;
+  return dueDate;
 }
 
-function moveByDays(submitDate, elapsedDays) {
-  let submitDateObj = new Date(submitDate);
-
+/**
+ *
+ * @param {number} dueDate - unix timestamp of the current due date
+ * @param {number} elapsedDays - number of full working days by which to advance the due date
+ * @returns {number} - the updated due date
+ * Examples:
+ * - moveByDays(Date.parse("March 10, 2025 12:05:36"), 2) -> (timestamp equivalent to March 12, 2025 12:05:36)
+ * - moveByDays(Date.parse("March 14, 2025 12:05:36"), 2) -> (timestamp equivalent to March 18, 2025 12:05:36)
+ */
+function moveByDays(dueDate, elapsedDays) {
   while (elapsedDays >= 5) {
     // For every 5 days we want to add, add 7
-    submitDate += 7 * 86400000;
+    dueDate += 7 * 86400000;
     elapsedDays -= 5;
   }
 
   // Add remaining elapsed days
   if (elapsedDays > 0) {
     // elapsedDays will be < 5, but we still need to check if we're crossing a weekend
-    let day = submitDateObj.getDay();
+    let day = new Date(dueDate).getDay();
     if (day + elapsedDays > 5) {
-      submitDate += (elapsedDays + 2) * 86400000;
+      dueDate += (elapsedDays + 2) * 86400000;
     } else {
-      submitDate += elapsedDays * 86400000;
+      dueDate += elapsedDays * 86400000;
     }
   }
-  return submitDate;
+  return dueDate;
 }
 
 /**
- * Usage:
- * calculateDueDate(submitDate, turnaround) -> Date | null
- * Input:
- *  - submitDate: Date and time the issue is submitted. Assume input is a unix timestamp,
- *      since a real ticket submission would likely use Date.now() or equivalent
- *  - turnaround: Lifetime of the issue, in hours
- * Output (Date):
- *  - Due date and time for the issue as a Date object
- * Example:
+ * Calculate due date
+ * @param {number} submitDate - unix timestamp of issue submission
+ * @param {number} turnaround - issue turnaround in hours
+ * @returns {Date} - the calculated due date as a Date object
+ * Examples:
  * - calculateDueDate(1741367136000, 19)
  * - calculateDueDate(Date.parse("March 10, 2025 12:05:36"), 19)
  *  => Date("March 12, 2025 15:05:36")
@@ -87,6 +107,7 @@ function calculateDueDate(submitDate, turnaround) {
     return null;
   }
 
+  let dueDate = submitDate;
   // Perform due date calculation
   // - Divide turnaround by 8 to get number of working days to add
   // - Add remainder
@@ -94,12 +115,12 @@ function calculateDueDate(submitDate, turnaround) {
   let remainingHours = turnaround % 8;
 
   // Move date by remaining hours
-  submitDate = moveByRemainingHours(submitDate, remainingHours);
+  dueDate = moveByRemainingHours(dueDate, remainingHours);
 
   // Add elapsed weeks
-  submitDate = moveByDays(submitDate, elapsedDays);
+  dueDate = moveByDays(dueDate, elapsedDays);
 
-  return new Date(submitDate);
+  return new Date(dueDate);
 }
 
 module.exports = {
